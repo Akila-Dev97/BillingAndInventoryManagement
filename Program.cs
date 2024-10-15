fba55fd70d37a6eb24966fd0933e6ec93884b42c
Akila Wijerathna <akilawijerathna5@gmail.com>
2024-10-15 20:40:41 +0530

    add connection string

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace AnyStore
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmAdminDashboard());
+            Application.Run(new frmUserDashboard());
         }
     }
 }
